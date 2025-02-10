36507e75448c3282f97f6a89c3ecd50a082ba523
harf0741 <mohamedjllj09@gmail.com>
2025-02-10 20:18:09 +0200

    Add files via upload

---
+++ card.js
@@ -0,0 +1,5 @@
+let contentcard = document.getElementById("contentcard")
+let cardtemplate = document.getElementById("cardtemplate")
+
+let hoom1 = cardtemplate.content.clonenode(true)
+contentcard.appendChild(hoom1)
